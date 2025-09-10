212f30dcf8d0955e6c911254b73dded9132c0b7e
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-10 12:22:43 +0000

    (try) add rustacean

--- flake.nix
+++ flake.nix
@@ -81,6 +81,7 @@
               nvim-treesitter.withAllGrammars
               nvim-web-devicons
               persistence-nvim
+              rustaceanvim
               snacks-nvim
               vim-dirdiff
               vim-monokai-tasty
