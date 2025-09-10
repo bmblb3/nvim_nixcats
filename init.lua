a3e1976abf28a088de3c72ac1eb1fb47acaead95
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-10 12:17:42 +0000

    spider don't skip insignificant punctuations

--- init.lua
+++ init.lua
@@ -341,6 +341,7 @@ hipatterns.setup({
   },
 })
 
+require("spider").setup({ skipInsignificantPunctuation = false })
 map({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
 map({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
 map({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
